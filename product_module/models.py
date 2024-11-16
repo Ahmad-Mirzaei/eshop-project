17d16c2c47f5aabb01c0e96b4750a4d6545e5965
ahmad <ahmad@yahoo.com>
2024-11-16 21:02:58 +0330

    37. changing model and migrate again

--- product_module/models.py
+++ product_module/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length = 300, verbose_name = "عنوان")
     price = models.IntegerField(verbose_name = "قیمت")
+    rating = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(5)], default = 0, verbose_name = "امتیاز")
+    short_description = models.CharField(max_length = 360, null = True, verbose_name = "توضیحات خلاصه")
+    is_active = models.BooleanField(default = False, verbose_name = "وضعیت")
 
+    def __str__(self):
+        return f"{self.title} ({self.price})"
 
