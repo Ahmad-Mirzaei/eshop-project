c15dc0db4debd5164b0edaf7c87041951c090526
ahmad <ahmad@yahoo.com>
2024-11-16 19:53:24 +0330

    36. working with data and datebase

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# static
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
-]
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
-
+# media
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
