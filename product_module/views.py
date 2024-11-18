db8a7f66b5af0cda2e7bc84e9149ff3ab8e0d471
giluyeh <giluyeh@yahoo.com>
2024-11-18 21:58:26 +0330

    46. aggregate and count in django

--- product_module/views.py
+++ product_module/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.http import Http404
+from django.db.models import Avg
 # Create your views here.
 
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'product_module/product_list.html', {'products': products})
+    products = Product.objects.all().order_by("price")  # براساس کمترین قیمت مرتب کن
+    number_of_products = products.count()
+    avg_rating = products.aggregate(Avg("rating"))
+    context = {
+        'products': products,
+        'total_number_of_products': number_of_products,
+        'avg_rating': avg_rating,
+    }
+    return render(request, 'product_module/product_list.html', context)
 
 def product_detail(request, slug):
     # step 1
