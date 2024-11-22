17aef44f3afb61b7a9a926123ad487cbdfda8668
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-22 16:03:08 +0330

    62. render partial in django

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product_module',
     'home_module',
+    'product_module',
+    'django_render_partial',
 
 ]
 
