5cc543f84074d517970ab1db8afa404ad6dd563d
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-22 17:04:26 +0330

    63. show product list

--- product_module/urls.py
+++ product_module/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name = "product-list"),
-    path(route = '<slug:slug>/', view = views.product_detail, name = "product-detail"),
+    path('<slug:slug>/', views.product_detail, name = "product-detail"),
 
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -3,20 +3,17 @@ from .models import Product, ProductCategory
 from django.http import Http404
 from django.db.models import Avg
 # Create your views here.
-
+# ----------------------------------------------------------------------------------------------------------------------
 def product_list(request):
     products = Product.objects.all().order_by("price")  # براساس کمترین قیمت مرتب کن
-    number_of_products = products.count()
     context = {
         'products': products,
-        'total_number_of_products': number_of_products,
     }
     return render(request, 'product_module/product_list.html', context)
 
-
+# ----------------------------------------------------------------------------------------------------------------------
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug = slug)
     return render(request, 'product_module/product_detail.html', {"product": product})
 
-
-# for test git status --short
+# ----------------------------------------------------------------------------------------------------------------------
