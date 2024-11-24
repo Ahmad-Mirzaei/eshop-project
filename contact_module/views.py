c59d66628e77b1b0821ef28e375ba7419a9886e9
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-25 00:11:35 +0330

    67. Fetching information from the POST request and checking the get and post methods on the server

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
 # Create your views here.
 
 def contact_us_page(request):
+    if request.method == "POST":
+        return redirect(reverse("home_page"))
     return render(request, 'contact_module/contact_us_page.html')
 
