d3f2e748d3faa26aa41edf0d6c5fd82d2a602946
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-26 00:34:28 +0330

    70. review django forms (3)

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -4,10 +4,35 @@ from django import forms
 class ContactUsForm(forms.Form):
     full_name = forms.CharField(
         max_length = 50,
+        label = "نام و نام خانوادگی",
         error_messages = {
-            "required" : "لطفاَ نام و نام خانوادگی را وارد کنید"
+            'required' : "لطفاَ نام و نام خانوادگی را وارد کنید",
+            'max_length' : "نام و نام خانوادگی نمی تواند بیشتر از 50 کاراکتر باشد"
         },
-        label = "نام و نام خانوادگی")
-    email = forms.EmailField(widget = forms.EmailInput, label = "ایمیل")
-    subject = forms.CharField(label = "عنوان")
-    text = forms.CharField(widget = forms.Textarea, label = "متن پیام")
+        widget = forms.TextInput(attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'نام و نام خانوادگی'
+        })
+    )
+    email = forms.EmailField(
+        label = "ایمیل",
+        widget = forms.EmailInput(attrs ={
+            'class' : 'form-control',
+            'placeholder' : 'ایمیل'
+        })
+    )
+    subject = forms.CharField(
+        label = "عنوان",
+        widget = forms.TextInput(attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'عنوان'
+        })
+    )
+    text = forms.CharField(
+        label = "متن پیام",
+        widget = forms.Textarea(attrs = {
+            'class' : 'form-control',
+            'placeholder' : 'متن خود را بنویسید...',
+            'id' : 'message'
+        })
+    )
