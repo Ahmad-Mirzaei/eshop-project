bd8091adebe9d2caa3e79ea7c73ce5c1b2f9c1bd
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-26 21:30:25 +0330

    72. ModelForm customization and data storage

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -48,3 +48,30 @@ class ContactUsModelForm(forms.ModelForm):
         fields = ['full_name', 'email', 'title', 'message']
         # fields = '__all__'
         # exclude = ['response']
+        widgets = {
+            'full_name' : forms.TextInput(attrs = {
+                'class' : 'form-control'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control'
+            }),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'message': forms.TextInput(attrs={
+                'class': 'form-control',
+                'rows' : 5,
+                'id' : 'message'
+            })
+        }
+
+        labels = {
+            'full_name' : 'نام و نام خانوادگی شما',
+            'email' : 'ایمیل شما'
+        }
+
+        error_messages = {
+            'full_name' : {
+                'required' : 'کاربر گرامی، وارد کردن نام و نام خانوادگی الزامی است'
+            },
+        }
