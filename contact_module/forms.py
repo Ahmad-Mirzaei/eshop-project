f025066c46609d90a9b05ddb9fcaef61f1feb108
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-26 16:57:15 +0330

    71. Storing form information in the database and checking the Model Form concept

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,38 +1,50 @@
 from django import forms
+from .models import ContactUs
+
+from django.core import validators
 
 
 class ContactUsForm(forms.Form):
     full_name = forms.CharField(
-        max_length = 50,
-        label = "نام و نام خانوادگی",
-        error_messages = {
-            'required' : "لطفاَ نام و نام خانوادگی را وارد کنید",
-            'max_length' : "نام و نام خانوادگی نمی تواند بیشتر از 50 کاراکتر باشد"
+        label='نام و نام خانوادگی',
+        max_length=50,
+        error_messages={
+            'required': 'لطفا نام و نام خانوادگی خود را وارد کنید',
+            'max_length': 'نام و نام خانوادگی نمی تواند بیشتر از 50 کاراکتر باشد'
         },
-        widget = forms.TextInput(attrs = {
-            'class' : 'form-control',
-            'placeholder' : 'نام و نام خانوادگی'
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'نام و نام خانوادگی'
         })
     )
     email = forms.EmailField(
-        label = "ایمیل",
-        widget = forms.EmailInput(attrs ={
-            'class' : 'form-control',
-            'placeholder' : 'ایمیل'
+        label='ایمیل',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'ایمیل'
         })
     )
-    subject = forms.CharField(
-        label = "عنوان",
-        widget = forms.TextInput(attrs = {
-            'class' : 'form-control',
-            'placeholder' : 'عنوان'
+    title = forms.CharField(
+        label='عنوان',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'عنوان'
         })
     )
-    text = forms.CharField(
-        label = "متن پیام",
-        widget = forms.Textarea(attrs = {
-            'class' : 'form-control',
-            'placeholder' : 'متن خود را بنویسید...',
-            'id' : 'message'
+    message = forms.CharField(
+        label='متن پیام',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'متن پیام',
+            'rows': '5',
+            'id': 'message'
         })
     )
+
+
+class ContactUsModelForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ['full_name', 'email', 'title', 'message']
+        # fields = '__all__'
+        # exclude = ['response']
