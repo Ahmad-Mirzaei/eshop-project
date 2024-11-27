1fb0e4d768dc3e837c1ff19d05585e914466a7a7
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-27 16:18:23 +0330

    74. review ListView in django - from django.views.generic import ListView

--- home_module/views.py
+++ home_module/views.py
@@ -3,12 +3,17 @@ from django.views import View
 from django.views.generic.base import TemplateView
 # Create your views here.
 
-# class HomeView(View):
-#     def get(self, request):
-#         return render(request, 'home_module/index_page.html')
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home_module/index_page.html')
 
-class HomeView(TemplateView):
-    template_name = 'home_module/index_page.html'
+# class HomeView(TemplateView):
+#     template_name = 'home_module/index_page.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['data'] = "this is message in home page"
+#         return context
 
 
 def site_header_component(request):
