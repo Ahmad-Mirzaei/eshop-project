2c10ba578298ce559746a99688c4fd42c5912eba
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-28 20:17:41 +0330

    77. review upload files in django

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import ContactUsModelForm
+from .forms import ContactUsModelForm, ProfileForm
 from django.views import View
 from django.views.generic.edit import FormView, CreateView
-from .models import ContactUs
+from .models import ContactUs,UserProfile
 # Create your views here.
 
 # step 1 with get and post method
@@ -41,10 +41,22 @@ class ContactUsView(CreateView):
 
 
 
+
+def store_file(file):
+    with open('temp/image.jpg', 'wb+') as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, 'contact_module/create_profile_page.html')
+        form = ProfileForm()
+        return render(request, 'contact_module/create_profile_page.html', {'form' : form})
 
     def post(self, request):
-        print(request.FILES)  # output -->  <MultiValueDict: {'image': [<InMemoryUploadedFile: 0bQpq.png (image/png)>]}>
-        return redirect('/contact-us/create-profile')
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            # store_file(request.FILES['profile'])
+            profile = UserProfile(image = request.FILES["user_image"])
+            profile.save()
+            return redirect('/contact-us/create-profile')
+        return render(request, 'contact_module/create_profile_page.html', {'form': submitted_form})
