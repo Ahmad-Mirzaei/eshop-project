a08131e8543afbef7e872790a2c2f623c5382dab
ahmad <ahmadmirzaei.py@gmail.com>
2024-11-28 15:25:22 +0330

    76. CreateView class base view

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,30 +1,43 @@
 from django.shortcuts import render, redirect
 from .forms import ContactUsModelForm
 from django.views import View
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
+from .models import ContactUs
 # Create your views here.
 
-class ContactUsView(FormView):
-    template_name = 'contact_module/contact_us_page.html'
+# step 1 with get and post method
+# class ContactUsView(View):
+#     def get(self, request):
+#         contact_form = ContactUsModelForm()
+#         return render(request, 'contact_module/contact_us_page.html', {
+#             'contact_form': contact_form
+#         })
+#     def post(self, request):
+#         contact_form = ContactUsModelForm(request.POST)
+#         if contact_form.is_valid():
+#             contact_form.save()
+#             return redirect('home_page')
+#         return render(request, 'contact_module/contact_us_page.html', {
+#             'contact_form': contact_form
+#         })
+
+
+# step 2 with FormView
+# class ContactUsView(FormView):
+#     template_name = 'contact_module/contact_us_page.html'
+#     form_class = ContactUsModelForm
+#     success_url = '/contact-us/'
+#
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+
+# step 3 with CreateView
+class ContactUsView(CreateView):
     form_class = ContactUsModelForm
+    template_name = 'contact_module/contact_us_page.html'
     success_url = '/contact-us/'
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
 
-
-    # def get(self, request):
-    #     contact_form = ContactUsModelForm()
-    #     return render(request, 'contact_module/contact_us_page.html', {
-    #         'contact_form': contact_form
-    #     })
-    # def post(self, request):
-    #     contact_form = ContactUsModelForm(request.POST)
-    #     if contact_form.is_valid():
-    #         contact_form.save()
-    #         return redirect('home_page')
-    #     return render(request, 'contact_module/contact_us_page.html', {
-    #         'contact_form': contact_form
-    #     })
 
