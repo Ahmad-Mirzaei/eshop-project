8a87910eb6d640ab0efbdd64b9ebbf5ddc9c1b54
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-18 21:33:29 +0330

    91. create AboutView class in home module

--- home_module/views.py
+++ home_module/views.py
@@ -32,3 +32,11 @@ def site_footer_component(request):
     return render(request, 'shared/site_footer_component.html', context)
 
 
+class AboutView(TemplateView):
+    template_name = 'home_module/about_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        site_setting: SiteSetting = SiteSetting.objects.filter(is_main_setting = True).first()
+        context["site_setting"] = site_setting
+        return context
