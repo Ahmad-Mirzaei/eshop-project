d1fff00231bda7ba710dbda48a9c9eb9eb63be81
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-18 19:41:15 +0330

    91. Contact information

--- contact_module/views.py
+++ contact_module/views.py
@@ -2,14 +2,20 @@ from django.views.generic import ListView
 
 from .forms import ContactUsModelForm
 from django.views.generic.edit import CreateView
-
 from .models import UserProfile
+from site_module.models import SiteSetting
 
 
 class ContactUsView(CreateView):
     form_class = ContactUsModelForm
     template_name = 'contact_module/contact_us_page.html'
     success_url = '/contact-us/'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        setting: SiteSetting = SiteSetting.objects.filter(is_main_setting = True).first()
+        context["site_setting"] = setting
+        return context
 
 
 def store_file(file):
