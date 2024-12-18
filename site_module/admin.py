085ac6820e4f0eae46ee9eea89760aab9e913ff7
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-18 20:37:19 +0330

    91. create FooterLinkBox & FooterLink models

--- site_module/admin.py
+++ site_module/admin.py
@@ -3,4 +3,13 @@ from . import models
 # Register your models here.
 
 
+@admin.register(models.FooterLink)
+class FooterLinkAdmin(admin.ModelAdmin):
+    list_display = ["title", "url"]
+
+
 admin.site.register(models.SiteSetting)
+
+admin.site.register(models.FooterLinkBox)
+
+# admin.site.register(models.FooterLink)
