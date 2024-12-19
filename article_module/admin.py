b757c6a4cee015c66c1a27dc025449cc7bc4c92b
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-19 21:40:00 +0330

    93. create ArticleCategiry model

--- article_module/admin.py
+++ article_module/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.ArticleCategory)
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url_title', 'parent', 'is_active']
+    list_editable = ['url_title', 'parent', 'is_active']
+
+
--- article_module/models.py
+++ article_module/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class ArticleCategory(models.Model):
+    parent = models.ForeignKey('ArticleCategory', on_delete = models.CASCADE, null = True, blank = True, verbose_name = "دسته بندی والد")
+    title = models.CharField(max_length = 200, verbose_name = "عنوان دسته بندی")
+    url_title = models.CharField(max_length = 300, unique = True, verbose_name = "عنوان در url")
+    is_active = models.BooleanField(default = True, verbose_name = "فعال / غیرفعال")
+
+    class Meta:
+        verbose_name = "دسته بندی مقاله"
+        verbose_name_plural = "دسته بندی های مقالات"
+
+    def __str__(self):
+        return self.title
+
+
+
