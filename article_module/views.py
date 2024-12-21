e02f4ebbc6db0a92be0790f1c7a11c74d2000738
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-21 17:38:43 +0330

    97. fix bug in distutils with install setuptools

--- article_module/views.py
+++ article_module/views.py
@@ -3,6 +3,7 @@ from .models import Article
 from django.views.generic.list import ListView
 from django.views import View
 from jalali_date import datetime2jalali, date2jalali
+import datetime
 # Create your views here.
 
 
@@ -18,3 +19,7 @@ class ArticlesView(ListView):
     paginate_by = 5
     template_name = 'article_module/articles_page.html'
 
+    def get_context_data(self,*args, **kwargs):
+        context = super(ArticlesView, self).get_context_data(*args, **kwargs)
+        context['date'] = date2jalali(self.request.user.date_joined)
+        return context
