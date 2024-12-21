e063b67c6784855f2c82cec8275dbe61e34cc37b
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-21 14:32:50 +0330

    96. add author firld in Article class

--- article_module/models.py
+++ article_module/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from account_module.models import User
 # Create your models here.
 
 class ArticleCategory(models.Model):
@@ -25,6 +25,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name = "متن مقاله")
     is_active = models.BooleanField(default = True, verbose_name = "فعال / غیرفعال")
     selected_categories = models.ManyToManyField(ArticleCategory, verbose_name = "دسته بندی ها")
+    author = models.ForeignKey(User, on_delete = models.CASCADE, null = True, verbose_name = "نویسنده")
 
     class Meta:
         verbose_name = "مقاله"
