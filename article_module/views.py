6883b25acbdb7d2602c2403446cedf3a862f52fe
ahmad <ahmadmirzaei.py@gmail.com>
2024-12-22 15:12:41 +0330

    98. create article_categories_component function

--- article_module/views.py
+++ article_module/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, ArticleCategory
 from django.views.generic.list import ListView
 from django.views import View
 from jalali_date import datetime2jalali, date2jalali
 import datetime
+from django.http import HttpRequest
 # Create your views here.
 
 
@@ -16,10 +17,15 @@ import datetime
 
 class ArticlesView(ListView):
     model = Article
-    paginate_by = 5
+    paginate_by = 10
     template_name = 'article_module/articles_page.html'
 
     def get_context_data(self,*args, **kwargs):
         context = super(ArticlesView, self).get_context_data(*args, **kwargs)
-        context['date'] = date2jalali(self.request.user.date_joined)
         return context
+
+
+def article_categories_component(request: HttpRequest):
+    article_main_categories = ArticleCategory.objects.filter(is_active = True, parent_id = None)
+    context = {'main_categories' : article_main_categories}
+    return render(request, 'article_module/components/article_categories_component.html', context)
