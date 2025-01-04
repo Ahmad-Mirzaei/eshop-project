0581a7c95f14fd58bd2a58d50c31d305a3ba2e40
ahmad <ahmadmirzaei.py@gmail.com>
2025-01-04 16:34:51 +0330

    105. edit codes

--- article_module/views.py
+++ article_module/views.py
@@ -1,55 +1,51 @@
-from django.core import paginator
-from django.shortcuts import render
-from .models import Article, ArticleCategory, ArticlesComment
-from django.views.generic.list import ListView
-from django.views.generic import DetailView
-from django.views import View
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views.generic import DetailView
+from django.views.generic.list import ListView
+from article_module.models import Article, ArticleCategory, ArticleComment
 
 
-# Create your views here.
-
-
-class ArticlesView(ListView):
+class ArticlesListView(ListView):
     model = Article
     paginate_by = 4
     template_name = 'article_module/articles_page.html'
 
-    def get_context_data(self,*args, **kwargs):
-        context = super(ArticlesView, self).get_context_data(*args, **kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticlesListView, self).get_context_data(*args, **kwargs)
         return context
 
     def get_queryset(self):
-        query = super(ArticlesView, self).get_queryset()
-        query = query.filter(is_active = True).order_by('-create_date')
+        query = super(ArticlesListView, self).get_queryset()
+        query = query.filter(is_active=True)
         category_name = self.kwargs.get('category')
         if category_name is not None:
-            query = query.filter(selected_categories__url_title__iexact = category_name)
+            query = query.filter(selected_categories__url_title__iexact=category_name)
         return query
 
 
-
-class ArticlesDetailView(DetailView):
+class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_module/article_detail_page.html'
 
     def get_queryset(self):
-        query = super(ArticlesDetailView, self).get_queryset()
-        query = query.filter(is_active = True)
+        query = super(ArticleDetailView, self).get_queryset()
+        query = query.filter(is_active=True)
         return query
 
     def get_context_data(self, **kwargs):
-        context = super(ArticlesDetailView, self).get_context_data()
-        article : Article = kwargs.get('object')
-        context['comments'] = ArticlesComment.objects.filter(article_id=article.id, parent=None).order_by('-create_date').prefetch_related('articlescomment_set')
-        context['comments_count'] = ArticlesComment.objects.filter(article_id=article.id).count()
+        context = super(ArticleDetailView, self).get_context_data()
+        article: Article = kwargs.get('object')
+        context['comments'] = ArticleComment.objects.filter(article_id=article.id, parent=None).order_by('-create_date').prefetch_related('articlecomment_set')
+        context['comments_count'] = ArticleComment.objects.filter(article_id=article.id).count()
         return context
 
 
-
 def article_categories_component(request: HttpRequest):
-    article_main_categories = ArticleCategory.objects.filter(is_active = True, parent_id = None)
-    context = {'main_categories' : article_main_categories}
+    article_main_categories = ArticleCategory.objects.filter(is_active=True, parent_id=None)
+
+    context = {
+        'main_categories': article_main_categories
+    }
     return render(request, 'article_module/components/article_categories_component.html', context)
 
 
@@ -58,11 +54,14 @@ def add_article_comment(request: HttpRequest):
         article_id = request.GET.get('article_id')
         article_comment = request.GET.get('article_comment')
         parent_id = request.GET.get('parent_id')
-        new_comment = ArticlesComment(article_id=article_id, text=article_comment, user_id=request.user.id, parent_id=parent_id)
+        print(article_id, article_comment, parent_id)
+        new_comment = ArticleComment(article_id=article_id, text=article_comment, user_id=request.user.id, parent_id=parent_id)
         new_comment.save()
         context = {
-            "comments" : ArticlesComment.objects.filter(article_id=article_d, parent=None).order_by('-create_date').prefetch_related('articlescomment_set'),
-            "comments_count" : ArticlesComment.objects.filter(article_is=article_is).count()
+            'comments': ArticleComment.objects.filter(article_id=article_id, parent=None).order_by('-create_date').prefetch_related('articlecomment_set'),
+            'comments_count': ArticleComment.objects.filter(article_id=article_id).count()
         }
+
         return render(request, 'article_module/includes/article_comments_partial.html', context)
-    return HttpResponse("response")
+
+    return HttpResponse('response')
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,7 @@
-function sendArticleComment(articleId){
+function sendArticleComment(articleId) {
     var comment = $('#commentText').val();
     var parentId = $('#parent_id').val();
+    console.log(parentId);
     $.get('/articles/add-article-comment', {
         article_comment: comment,
         article_id: articleId,
@@ -9,19 +10,16 @@ function sendArticleComment(articleId){
         $('#comments_area').html(res);
         $('#commentText').val('');
         $('#parent_id').val('');
+
         if (parentId !== null && parentId !== '') {
-            document.getElementById('single_comment_box').scrollIntoView({behavior: "smooth"})
-        }
-        else {
-            document.getElementById('comments_area').scrollIntoView({behavior: "smooth"})
+            document.getElementById('single_comment_box_' + parentId).scrollIntoView({behavior: "smooth"});
+        } else {
+            document.getElementById('comments_area').scrollIntoView({behavior: "smooth"});
         }
     });
 }
 
-
 function fillParentId(parentId) {
     $('#parent_id').val(parentId);
-    document.getElementById('comment_form').scrollIntoView({behavior: "smooth"})
+    document.getElementById('comment_form').scrollIntoView({behavior: "smooth"});
 }
-
-
