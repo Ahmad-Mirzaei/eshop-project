041ca1962ef0bfdd491872c823283ce18801fc46
ahmad <ahmadmirzaei.py@gmail.com>
2025-01-07 14:31:36 +0330

    106. edit all

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -2,24 +2,24 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-# Create your views here.
+from .forms import EditProfileModelForm
 
 
 class UserPanelDashboardPage(TemplateView):
     template_name = 'user_panel_module/user_panel_dashboard_page.html'
 
 
-
 class EditUserProfilePage(View):
     def get(self, request: HttpRequest):
-        context = {}
-        return render(request, "user_panel_module/edit_profile_page.html", context)
+        edit_form = EditProfileModelForm()
+        context = {
+            'form': edit_form
+        }
+        return render(request, 'user_panel_module/edit_profile_page.html', context)
 
     def post(self, request: HttpRequest):
-        context = {}
-        return render(request, "user_panel_module/edit_profile_page.html", context)
-
+        return render(request, 'user_panel_module/edit_profile_page.html', {})
 
 
 def user_panel_menu_component(request: HttpRequest):
-    return render(request, "user_panel_module/components/user_panel_menu_component.html")
+    return render(request, 'user_panel_module/components/user_panel_menu_component.html')
