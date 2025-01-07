19c3a25c5bff7d6c1143320ab541812db94858bc
ahmad <ahmadmirzaei.py@gmail.com>
2025-01-07 13:34:33 +0330

    106. add user_panel_menu_component in views / create user_panel_menu_component html / edit user_panel_dashboard_page html

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +7,9 @@ from django.views.generic import TemplateView
 
 class UserPanelDashboardPage(TemplateView):
     template_name = 'user_panel_module/user_panel_dashboard_page.html'
+
+
+
+def user_panel_menu_component(request: HttpRequest):
+
+    return render(request, "user_panel_module/components/user_panel_menu_component.html")
