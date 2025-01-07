a7220f22cab636f3b3d4b455ef1955c5190c1d02
ahmad <ahmadmirzaei.py@gmail.com>
2025-01-07 13:47:07 +0330

    add EditUserProfilePage in user_panel_module

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-
-# Create your views here.
+from django.views import View
 from django.views.generic import TemplateView
+# Create your views here.
 
 
 class UserPanelDashboardPage(TemplateView):
     template_name = 'user_panel_module/user_panel_dashboard_page.html'
 
 
+class EditUserProfilePage(View):
+    def get(self, request: HttpRequest):
+        context = {}
+        return render(request, "user_panel_module/edit_profile_page.html", context)
+
+    def post(self, request: HttpRequest):
+        context = {}
+        return render(request, "user_panel_module/edit_profile_page.html", context)
+
 
 def user_panel_menu_component(request: HttpRequest):
-
     return render(request, "user_panel_module/components/user_panel_menu_component.html")
